5f4d56fec9fffa6d2bd14c19bf329ec7440ee0f6
System Administrator <root@ympc.local>
2015-12-18 12:15:40 +0900

    Install FuelPHP

---
+++ fuel/app/logs/2015/12/18.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+ERROR - 2015-12-18 02:48:25 --> Error - date_default_timezone_get(): It is not safe to rely on the system's timezone settings. You are *required* to use the date.timezone setting or the date_default_timezone_set() function. In case you used any of those methods and you are still getting this warning, you most likely misspelled the timezone identifier. We selected the timezone 'UTC' for now, but please set date.timezone to select your timezone. in /Library/WebServer/Documents/fuel_form/fuel/core/classes/fuel.php on line 162
